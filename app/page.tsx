f2b9d87280419f65f7aa77d872a76db066f93df4
stipe-jurkovic <sjurko00@fesb.hr>
2024-09-02 23:03:20 +0200

    made contentful for the latest as well

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
 import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
+import { fetchEntries } from '../lib/contentful'
+import { Entry } from 'contentful'
 
 export default async function Page() {
+  const entries = await fetchEntries()
+  const postsContentful = entries
+    .filter((entry: Entry<any>) => entry.fields.mdxSource)
+    .map((entry: Entry<any>) => entry.fields)
+    .sort((a: any, b: any) => {
+      return new Date(b.date).getDate() - new Date(a.date).getDate()
+    })
+  const postings: {
+    path: string
+    date: string
+    title: string
+    summary: string
+    tags: string[]
+  }[] = []
+  postsContentful.map((post) => {
+    const posting = {
+      path: (post.link as string)?.slice(1),
+      date: post.date as string,
+      title: post.title as string,
+      summary: post.description as string,
+      tags: post.tags as string[],
+    }
+    postings.push(posting)
+  })
   const sortedPosts = sortPosts(allBlogs)
   const posts = allCoreContent(sortedPosts)
-  return <Main posts={posts} />
+  return <Main posts={postings} />
 }
